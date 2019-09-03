b86da3df5c5f54e4600fc4e30ed7cdf23f23d18e
stevealbertwong <wongkwunkit@gmail.com>
2019-09-03 04:42:15 -0700

    fix bug

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -339,12 +339,14 @@ start_process (void *full_cmdline)
     // printf("process.c start_process() after sema_up \n");
   }
   
+  printf("process.c load() before assembly \n");
   // 6. kernel "interrupt switch" to user ps  
   // "assembly start" ps by simulating a return from interrupt i.e. jmp intr_exit(&if)
   // intr_exit() passes intr_frame{}/stack_frame to user ps 
   // pop to segment registers : intr_frame{}->%esp == cmdline stored on user_stack
   asm volatile ("movl %0, %%esp; jmp intr_exit" : : "g" (&if_) : "memory");
-  
+  printf("process.c load() after assembly \n");
+
   palloc_free_page(cmdline_tokens);
   NOT_REACHED ();
 }
