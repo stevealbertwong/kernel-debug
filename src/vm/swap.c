d447ef4199bde3ddff9ff683efc4fe140922c5d0
stevealbertwong <wongkwunkit@gmail.com>
2019-10-14 06:33:42 -0700

    fix bug

--- src/vm/swap.c
+++ src/vm/swap.c
@@ -15,6 +15,7 @@ static struct bitmap *free_swap_disk_pages;
 static struct block *swap_disk_partition; // device driver object in kernel pool
 static const size_t SECTORS_PER_PAGE = PGSIZE/BLOCK_SECTOR_SIZE;//8:4096/512
 static struct lock swap_lock;
+size_t swap_total_pages;
 
 /**
  * called in init.c main()
@@ -34,7 +35,7 @@ void vm_swap_init(void){
         PANIC ("vm_swap_init() failed to get disk partition object \n");
     }
     // 2. init() bitmap according to disk partition object 
-    size_t swap_total_pages = block_size(swap_disk_partition) / SECTORS_PER_PAGE;
+    swap_total_pages = block_size(swap_disk_partition) / SECTORS_PER_PAGE;
     printf("swap_total_pages %d \n", swap_total_pages);
     free_swap_disk_pages = bitmap_create(swap_total_pages);
     if(!free_swap_disk_pages){
@@ -59,25 +60,25 @@ uint32_t vm_swap_flush_kpage_to_disk(void* kpage){
     lock_acquire (&swap_lock);
     printf("vm_swap_flush_kpage_to_disk() called \n");
     // 1. search() bitamp for free 1 disk sectors 
-    size_t free_swap_disk_sector = bitmap_scan (free_swap_disk_pages, 
+    size_t starting_disk_sector_for_kpage = bitmap_scan (free_swap_disk_pages, 
         /*start*/0, /*cnt*/1, true); // 1 bit == 1 disk page == 8 disk sectors
-    if(!free_swap_disk_sector){
+    if(starting_disk_sector_for_kpage < swap_total_pages){
         PANIC("vm_swap_flush_kpage_to_disk() failed, no more swap disk space left \n");
     }
     // 2. block_write() 1 kpage to swap disk
     size_t i;
     for (i = 0; i < SECTORS_PER_PAGE; ++ i) {//1 kpage == 8 disk sectors
         block_write (swap_disk_partition, // device driver object
-            free_swap_disk_sector * SECTORS_PER_PAGE + i, // disk sector number of 1 page
+            starting_disk_sector_for_kpage * SECTORS_PER_PAGE + i, // disk sector number of 1 page
             kpage + (BLOCK_SECTOR_SIZE * i) // src: buffer/kpage 
         );
     }
     
     // 3. u() bitamp
-    bitmap_set(free_swap_disk_pages, free_swap_disk_sector, true);
+    bitmap_set(free_swap_disk_pages, starting_disk_sector_for_kpage, true);
     lock_release (&swap_lock);
 
-    return free_swap_disk_sector;
+    return starting_disk_sector_for_kpage;
 }
 
 
