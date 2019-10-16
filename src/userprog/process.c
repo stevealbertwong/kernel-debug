97800f621eace46bba3cd350283738aa12b3128b
stevealbertwong <wongkwunkit@gmail.com>
2019-10-16 02:25:01 -0700

    fix bug

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -278,7 +278,7 @@ process_exit (void)
 
   // 1. child check if parent wait and unblock parent to get its exit_status
 	while (!list_empty(&exiting_thread->sema_parent_block_itself_wait_for_child_exit_status.waiters)){
-    printf("process.c process_exit() before sema_up, tid: %d\n", child_thread->tid);
+    printf("process.c process_exit() before sema_up, tid: %d\n", exiting_thread->tid);
     sema_up(&exiting_thread->sema_parent_block_itself_wait_for_child_exit_status);
     // printf("process.c process_exit() after sema_up \n");
   }
@@ -288,7 +288,7 @@ process_exit (void)
 	// 1. child block itself whether parent waits
   // for parent to: free() data structure + get exit_status if parent process_wait()
 	if (exiting_thread->parent != NULL){ // not orphan
-    printf("process.c process_exit() before sema_down, tid: %d\n", child_thread->tid);
+    printf("process.c process_exit() before sema_down, tid: %d\n", exiting_thread->tid);
 		sema_down(&exiting_thread->sema_child_block_itself_before_free);
     // printf("process.c process_exit() after sema_down \n");
   }
