089a2c334e31eea77f9274cae6ce02ebc025b2cb
stevealbertwong <wongkwunkit@gmail.com>
2019-10-17 00:19:53 -0700

    fix bug

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -277,9 +277,10 @@ thread_exit (void)
 
 #ifdef VM  
   thread_current()->freed = true;
-  // if (thread_current() != NULL && thread_current()->parent != NULL){
-  if (thread_current()->parent != initial_thread){
+  if (thread_current() != NULL && thread_current()->parent != NULL){
+    if (thread_current()->parent != initial_thread){
       list_remove(&thread_current()->children_threads_elem);      
+    }
   }
 #endif
 
