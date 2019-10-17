b343743c43d8deb66a363b861b0a0368b10e5a7a
stevealbertwong <wongkwunkit@gmail.com>
2019-10-17 02:20:22 -0700

    fix bug

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -320,7 +320,7 @@ process_exit (void)
 
   // 3. free() vm, fs data structure
   // destroy current thread's pagedir, switch to kernel only pagedir
-  // vm_free_supt_frame_swap (exiting_thread->supt);
+  vm_free_supt_frame_swap (exiting_thread->supt);
 
   // printf("process_exit() vm_free_supt_frame_swap !!! \n");  
 
