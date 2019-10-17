5b5e0f768552f0022143cf4e67d9a3dd8f932af4
stevealbertwong <wongkwunkit@gmail.com>
2019-10-17 01:15:48 -0700

    fix bug

--- src/threads/malloc.c
+++ src/threads/malloc.c
@@ -200,8 +200,7 @@ free (void *p)
         }
       else
         {
-          /* It's a big block.  Free its pages. */
-          printf("free() called \n");
+          /* It's a big block.  Free its pages. */          
           palloc_free_multiple (a, a->free_cnt);
           return;
         }
