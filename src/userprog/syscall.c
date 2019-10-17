5bf40da06e0063063f87857e1fcf7c3b4e2ff10d
stevealbertwong <wongkwunkit@gmail.com>
2019-10-17 00:23:08 -0700

    fix bug

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -284,6 +284,7 @@ void system_call_exit(int status)
 // #endif
 
 	t->elf_exit_status = status;	
+	printf("%s: exit(%d), tid: %d\n", t->name, t->elf_exit_status, t->tid);	
 	printf("%s: exit(%d)\n", t->name, t->elf_exit_status);	
 	thread_exit();
 }
